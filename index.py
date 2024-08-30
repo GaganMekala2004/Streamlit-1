6ca1f408dbf29558cb44332daac0fdc87c66dc33
GaganMekala2004 <149994699+GaganMekala2004@users.noreply.github.com>
2024-08-30 05:53:25 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.title("Hello world!")
+st.header("Hello world!")
+st.subheader("Hello world!")
+st.caption("Hello world!")
+st.text("Hello world!")
+st.write("Hello world2")
+st.code("print('hello world')")
